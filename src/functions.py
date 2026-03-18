6eedba28e3aed3c2d2aea8ffe5d4ff1368b0e618
Paraskevi Dimitrakou <pdimitrakou@gmail.com>
2026-03-18 23:06:41 +0200

    m_changes

--- src/functions.py
+++ src/functions.py
@@ -20,3 +20,43 @@ def stratified_split(data, test_size=0.2, seed=42):
     val = val.drop(columns=["age_bin"])
 
     return train, val
+
+
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+
+
+def build_cpg_pipeline():
+
+    pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler", StandardScaler())
+    ])
+
+    return pipeline
+
+
+
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+
+def build_preprocessor(cpg_cols, categorical_cols):
+
+    cpg_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler", StandardScaler())
+    ])
+
+    categorical_pipeline = OneHotEncoder(handle_unknown="ignore")
+
+    preprocessor = ColumnTransformer([
+        ("cpg", cpg_pipeline, cpg_cols),
+        ("cat", categorical_pipeline, categorical_cols)
+    ])
+
+    return preprocessor
